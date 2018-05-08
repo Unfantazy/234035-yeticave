cff877df43833be68ba468688ec4ff53a9de40be
Evgeniy Kobrikov <kobrikov@yandex.ru>
2018-05-08 20:42:44 +0300

    Fixed remarks

--- schema.sql
+++ schema.sql
@@ -4,46 +4,51 @@ CREATE DATABASE yeticave
 
 USE yeticave;
 
-CREATE TABLE user (
+SET FOREIGN_KEY_CHECKS=0;
+
+CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   reg_date DATETIME,
-  email CHAR(128),
-  name CHAR(64),
-  password CHAR(64),
-  avatar TEXT,
-  contact CHAR(128),
-  lot_id INT,
-  bet_id INT
-);
+  email VARCHAR(64),
+  name VARCHAR(64),
+  password VARCHAR(64),
+  avatar VARCHAR(128),
+  contact VARCHAR(128)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE category (
+CREATE TABLE categories (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name CHAR(64)
-);
+  name VARCHAR(64)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE bet (
+CREATE TABLE bets (
   id INT AUTO_INCREMENT PRIMARY KEY,
   date DATETIME,
   amount INT,
   user_id INT,
-  lot_id INT
-);
+  lot_id INT,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (lot_id) REFERENCES lots(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE lot (
+CREATE TABLE lots (
   id INT AUTO_INCREMENT PRIMARY KEY,
   creation_date DATETIME,
-  name CHAR,
-  description CHAR,
-  image TEXT,
+  name VARCHAR(64),
+  description VARCHAR(128),
+  image VARCHAR(128),
   initial_price INT,
   completion_date DATETIME,
   step_lot INT,
   category_id INT,
   author_id INT,
-  winner_id INT
-);
+  winner_id INT,
+  FOREIGN KEY (category_id) REFERENCES categories(id),
+  FOREIGN KEY (author_id) REFERENCES users(id),
+  FOREIGN KEY (winner_id) REFERENCES users(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE INDEX lot ON lot(category_id);
+CREATE INDEX lots ON lots(category_id);
 
-CREATE UNIQUE INDEX name ON lot(name);
-CREATE UNIQUE INDEX email ON user(email);
+CREATE UNIQUE INDEX name ON lots(name);
+CREATE UNIQUE INDEX email ON users(email);
